9c9c5420feb21adc269d2f90ac8d93c6023e39a0
piotreq20 <75125885+piotreq20@users.noreply.github.com>
2020-12-02 13:45:23 +0100

    Add files via upload

---
+++ Ćwiczenia1/main.java
@@ -0,0 +1,12 @@
+package Ćwiczenia1;
+
+public class main {
+    public static void main(String[] args) {
+
+
+        for (int i=0; i<=10; i++){
+            if(i%2 == 0) System.out.print(i + ", ");
+        }
+
+    }
+}
