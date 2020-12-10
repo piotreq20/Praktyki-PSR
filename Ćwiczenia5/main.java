3746f923738ba0d26c48ad82dc65eaa38c22e5e9
piotreq20 <75125885+piotreq20@users.noreply.github.com>
2020-12-10 23:33:26 +0100

    Add files via upload

---
+++ Ćwiczenia5/main.java
@@ -0,0 +1,9 @@
+package Ćwiczenia5;
+
+public class main {
+    public static void main(String[] args) {
+
+        if (Math.random() < 0.5) System.out.println("Orzeł");
+        else                     System.out.println("Reszka");
+    }
+}
